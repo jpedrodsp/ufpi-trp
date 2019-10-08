f8383f9486e914065185fbf5038e2d92cd16b7d8
João Pedro Patrocinio <jpedro9966@gmail.com>
2019-10-08 15:27:56 -0300

    Fixed timeout function. Received data is now decoded.

--- src/trp/trp_client.py
+++ src/trp/trp_client.py
@@ -17,6 +17,7 @@ class TRP_client:
 
     def send(self, data, target_host, target_port):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.settimeout(trp_defaults.default_timeout)
         _address_tuple = (target_host, target_port)
 
         new_packet = trp_packet.create_packet(self.packet_count, data=data.encode('utf-8'))
@@ -24,8 +25,14 @@ class TRP_client:
         retry_timer = 1
         while not packet_acknowledged:
             #print("waiting for packet", new_packet.id)
-            self._socket.sendto(str(new_packet).encode('utf-8'), _address_tuple)
-            resp, addr = self._socket.recvfrom(trp_defaults.default_buffer_max_size)
+            while True:
+                try:
+                    self._socket.sendto(str(new_packet).encode('utf-8'), _address_tuple)
+                    resp, addr = self._socket.recvfrom(trp_defaults.default_buffer_max_size)
+                    break
+                except socket.timeout:
+                    print("Client timeout. Retrying")
+                    pass
             #print("received from server", resp)
             if trp_packet.convert_text_to_ack(resp.decode('utf-8')):
                 packet_acknowledged = True
--- src/trp/trp_defaults.py
+++ src/trp/trp_defaults.py
@@ -1,2 +1,3 @@
 default_host_port = 6900
+default_timeout = 2
 default_buffer_max_size = 1024
